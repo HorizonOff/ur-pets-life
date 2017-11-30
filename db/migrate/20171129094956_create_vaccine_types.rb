f4b0d85b24e558760d07a7609c68bebaa5acd7f4
igor_kulich <ihor2405@gmail.com>
2017-11-30 13:54:01 +0200

    Fix megrations

--- db/migrate/20171129094956_create_vaccine_types.rb
+++ db/migrate/20171129094956_create_vaccine_types.rb
@@ -2,9 +2,10 @@ class CreateVaccineTypes < ActiveRecord::Migration[5.1]
   def change
     create_table :vaccine_types do |t|
       t.string :name
-      t.text :pet_categories, array:true, default: []
 
       t.timestamps
     end
+
+    create_join_table :pet_types, :vaccine_types
   end
 end
