ed95f51512c958fb87bc982ef6dbbca5337bdca0
igor_kulich <ihor2405@gmail.com>
2018-01-16 17:56:36 +0200

    Change lost/found/adoption logic

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -9,18 +9,24 @@ class Pet < ApplicationRecord
   has_many :vaccinations, dependent: :destroy
   has_many :pictures, dependent: :destroy
   has_many :appointments, dependent: :destroy
-  has_many :past_clinic_appointments, -> { where(bookable_type: 'Clinic').past }, class_name: Appointment
+  has_many :past_clinic_appointments, -> { where(bookable_type: 'Clinic').past }, class_name: 'Appointment'
 
-  has_one :location, through: :user
+  has_one :location, as: :place, inverse_of: :place
   reverse_geocoded_by 'locations.latitude', 'locations.longitude'
 
+  delegate :address, to: :location
+
+  accepts_nested_attributes_for :location, update_only: true
   accepts_nested_attributes_for :vaccinations, allow_destroy: true
   accepts_nested_attributes_for :pictures, allow_destroy: true
 
-  validates_presence_of :name, message: 'Name is required'
-  validates_presence_of :birthday, message: 'Birthday is required'
-  validates_presence_of :sex, message: 'Sex is required'
-  validates_presence_of :additional_type, message: 'Type is required', if: :additiona_type_required?
+  validates_presence_of :name, message: 'Name is required', if: :not_found?
+  validates_presence_of :birthday, message: 'Birthday is required', if: :not_found?
+  validates_presence_of :sex, message: 'Sex is required', if: :not_found?
+  validates_presence_of :additional_type, message: 'Type is required', if: :additional_type_required?
+
+  validates_presence_of :description, message: 'Short description is required', if: :lost_or_found?
+  validates_presence_of :mobile_number, message: 'Mobile number is required', if: :lost_or_found?
 
   validate :sex_should_be_valid, :breed_should_be_valid, :lost_and_found_should_be_vaild
   # validates :avatar, file_size: { less_than: 1.megabyte }
@@ -28,10 +34,13 @@ class Pet < ApplicationRecord
 
   has_paper_trail only: [:weight], skip: [:avatar]
 
-  scope :alphabetical_order, -> { order(name: :asc) }
+  before_save :remove_location, if: ->(obj) { obj.changed_attributes.keys.include?('lost_at') && obj.lost_at.blank? }
+
+  scope :alphabetical_order, -> { order(description: :asc) }
   scope :for_adoption,       -> { where(is_for_adoption: true, lost_at: nil) }
   scope :lost,               -> { where.not(lost_at: nil) }
   scope :found,              -> { where.not(found_at: nil) }
+  scope :owned,              -> { where(found_at: nil) }
   scope :lost_or_found,      -> { lost.or(found) }
   scope :can_be_lost,        -> { where(lost_at: nil, found_at: nil) }
   scope :can_be_adopted,     -> { where(lost_at: nil, found_at: nil, is_for_adoption: false) }
@@ -48,27 +57,11 @@ class Pet < ApplicationRecord
   end
 
   def is_lost=(value)
-    if value.in? [true, 'true']
-      self.lost_at ||= Time.now
-    else
-      self.lost_at = nil
-    end
-  end
-
-  def is_lost
-    lost_at.present?
-  end
-
-  def is_found=(value)
-    if value.in? [true, 'true']
-      self.found_at ||= Time.now
-    else
-      self.lost_at = nil
-    end
-  end
-
-  def is_found
-    found_at.present?
+    return unless value.in? [false, 'false']
+    self.lost_at = nil
+    self.mobile_number = nil
+    self.description = nil
+    self.additional_comment = nil
   end
 
   def pet_type_is_additional?
@@ -77,8 +70,20 @@ class Pet < ApplicationRecord
 
   private
 
-  def additiona_type_required?
+  def remove_location
+    location.destroy if location.present?
+  end
+
+  def additional_type_required?
     pet_type_is_additional?
+  end
+
+  def not_found?
+    found_at.blank?
+  end
+
+  def lost_or_found?
+    lost_at.present? || found_at.present?
   end
 
   def sex_should_be_valid
@@ -89,7 +94,7 @@ class Pet < ApplicationRecord
   end
 
   def breed_should_be_valid
-    return if breed_id.nil? && pet_type_is_additional?
+    return if (breed_id.nil? && pet_type_is_additional?) || found_at.present?
     errors.add(:breed_id, 'Breed is invalid') unless Breed.exists?(pet_type_id: pet_type_id, id: breed_id)
   end
 
