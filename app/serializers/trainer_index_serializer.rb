e3cbb7c1d242cec3867f20a958d8c2514b0ab98a
igor_kulich <ihor2405@gmail.com>
2018-01-25 17:39:09 +0200

    Add favorite_id to trainers response

--- app/serializers/trainer_index_serializer.rb
+++ app/serializers/trainer_index_serializer.rb
@@ -1,3 +1,3 @@
 class TrainerIndexSerializer < BaseMethodsSerializer
-  attributes :id, :name, :picture_url, :address, :distance
+  attributes :id, :name, :picture_url, :address, :distance, :favorite_id
 end
