bb9a38dae3201261109844a347422e57e50c8013
igor_kulich <ihor2405@gmail.com>
2018-01-25 17:39:35 +0200

    Remove consultation_fee from favorites response

--- app/serializers/service_centre_index_serializer.rb
+++ app/serializers/service_centre_index_serializer.rb
@@ -1,3 +1,7 @@
 class ServiceCentreIndexSerializer < WorkingHoursSerializer
   attributes :id, :name, :picture_url, :working_hours, :address, :distance, :type, :favorite_id
+
+  def consultation_fee
+    object.consultation_fee if scope[:favorite].present?
+  end
 end
