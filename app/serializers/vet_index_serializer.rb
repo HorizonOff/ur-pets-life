a6963afc208519ce225fb0579a5f83507c0014d2
igor_kulich <ihor2405@gmail.com>
2018-01-25 18:20:40 +0200

    Add favorite_id to vets

--- app/serializers/vet_index_serializer.rb
+++ app/serializers/vet_index_serializer.rb
@@ -1,3 +1,3 @@
 class VetIndexSerializer < BaseMethodsSerializer
-  attributes :id, :name, :avatar_url, :experience, :consultation_fee, :pet_type_ids
+  attributes :id, :name, :avatar_url, :experience, :consultation_fee, :pet_type_ids, :favorite_id
 end
