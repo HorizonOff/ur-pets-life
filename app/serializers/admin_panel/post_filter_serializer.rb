e18678ae5edbf81d337f01f0063ee2ab184ed409
igor_kulich <ihor2405@gmail.com>
2018-02-12 15:33:01 +0200

    Fix Pets-91 Password validation

--- app/serializers/admin_panel/post_filter_serializer.rb
+++ app/serializers/admin_panel/post_filter_serializer.rb
@@ -1,5 +1,5 @@
 module AdminPanel
-  class PostFilterSerializer <  ActiveModel::Serializer
+  class PostFilterSerializer < ActiveModel::Serializer
     attributes :id, :title, :actions, :created_at
     attribute :pet_type_name, key: :pet_type_id
     attribute :user_name, key: :name
