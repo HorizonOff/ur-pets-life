03f004111196f5a84c93a3285b15bb3996c61ea5
igor_kulich <ihor2405@gmail.com>
2018-03-14 12:53:53 +0200

    Fix rubocop

--- app/controllers/admin_panel/admins/invitations_controller.rb
+++ app/controllers/admin_panel/admins/invitations_controller.rb
@@ -1,13 +1,15 @@
 module AdminPanel
-  class Admins::InvitationsController < Devise::InvitationsController
-    private
+  module Admins
+    class InvitationsController < Devise::InvitationsController
+      private
 
-    def after_invite_path_for(inviter, invitee = nil)
-      admin_panel_admins_path
-    end
+      def after_invite_path_for(_inviter, _invitee)
+        admin_panel_admins_path
+      end
 
-    def after_accept_path_for(resource)
-      admin_panel_root_path
+      def after_accept_path_for(_resource)
+        admin_panel_root_path
+      end
     end
   end
 end
