4327b1714cb138d2e1f737c13d22d5ddecad41a4
ihorkul <ihor2405@gmail.com>
2018-05-11 11:43:45 +0300

    Fix service option time form

--- app/models/service_option_time.rb
+++ app/models/service_option_time.rb
@@ -3,11 +3,11 @@ class ServiceOptionTime < ApplicationRecord
 
   acts_as_paranoid
 
-  before_validation :set_same_date, on: :create
+  before_validation :set_same_date, on: :create, unless: :some_date_blank?
 
   validates :start_at, :end_at, presence: true
 
-  validate :values_should_be_valid
+  validate :values_should_be_valid, unless: :some_date_blank?
 
   def time_range
     start_at.strftime('%l:%M %p').strip + ' - ' + end_at.strftime('%l:%M %p').strip
@@ -16,12 +16,15 @@ class ServiceOptionTime < ApplicationRecord
   private
 
   def set_same_date
-    return if start_at.blank? || end_at.blank?
     self.start_at = start_at.change(year: 2018, month: 1, day: 1)
     self.end_at = end_at.change(year: 2018, month: 1, day: 1)
   end
 
   def values_should_be_valid
     errors.add(:end_at, 'End time should be after start time') if start_at > end_at
+  end
+
+  def some_date_blank?
+    @some_date_blank ||= start_at.blank? || end_at.blank?
   end
 end
