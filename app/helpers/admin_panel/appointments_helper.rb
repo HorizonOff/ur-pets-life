2e3163d1802757680f291e3062bd697e156ac05f
Igor Kulich <ihor2405@gmail.com>
2018-06-28 17:00:33 +0300

    Fix code

--- app/helpers/admin_panel/appointments_helper.rb
+++ app/helpers/admin_panel/appointments_helper.rb
@@ -11,36 +11,38 @@ module AdminPanel
       end
     end
 
-    def appointment_actions(appointment, btn_class = '')
+    def appointment_actions(appointment)
       if appointment.pending?
         accept_reject_links(appointment)
       elsif appointment.accepted?
-        link_for_canceling(appointment, btn_class)
+        link_for_canceling(appointment)
       end
     end
+
+    private
 
     def accept_reject_links(appointment)
       if appointment.can_be_accepted?
-        (link_for_accepting(appointment, btn_class) + link_for_rejecting(appointment, btn_class))
+        (link_for_accepting(appointment) + link_for_rejecting(appointment))
       else
-        link_for_rejecting(appointment, btn_class)
+        link_for_rejecting(appointment)
       end
     end
 
-    def link_for_accepting(appointment, btn_class)
-      link_to accept_admin_panel_appointment_path(appointment), class: "btn btn-success #{btn_class}", method: :put do
+    def link_for_accepting(appointment)
+      link_to accept_admin_panel_appointment_path(appointment), class: 'btn btn-success', method: :put do
         content_tag(:i, nil, class: 'fa fa-check') + 'Accept'
       end
     end
 
-    def link_for_rejecting(appointment, btn_class)
-      link_to reject_admin_panel_appointment_path(appointment), class: "btn btn-danger #{btn_class}", method: :put do
+    def link_for_rejecting(appointment)
+      link_to reject_admin_panel_appointment_path(appointment), class: 'btn btn-danger', method: :put do
         content_tag(:i, nil, class: 'fa fa-close') + 'Reject'
       end
     end
 
-    def link_for_canceling(appointment, btn_class)
-      link_to cancel_admin_panel_appointment_path(appointment), class: "btn btn-danger #{btn_class}", method: :put do
+    def link_for_canceling(appointment)
+      link_to cancel_admin_panel_appointment_path(appointment), class: 'btn btn-danger', method: :put do
         content_tag(:i, nil, class: 'fa fa-close') + 'Cancel'
       end
     end
