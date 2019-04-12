cf83c9d0ab4aca09674e18e87e8ef8477edc5a3d
Yorkego <yorkego1@ukr.net>
2019-04-12 10:31:43 +0300

    Remove puts

--- app/controllers/admin_panel/orders_controller.rb
+++ app/controllers/admin_panel/orders_controller.rb
@@ -281,12 +281,9 @@ module AdminPanel
 
       @orders = Order.order(:id).includes({user: [:location]}, {order_items: [item: :item_brand]})
                                       .where("(users.id = (?) OR #{is_user_present}) AND order_status_flag = (?)", @@filtered_user_id, 'delivered').references(:user)
-      puts @orders.pluck(:id, :created_at)
       if params[:from_date].present? && params[:to_date].present?
         @orders = @orders.created_in_range(params[:from_date], params[:to_date].to_date.end_of_day)
       end
-      puts @orders.pluck(:id, :created_at)
-
       user_name = @@filtered_user_id > 0 ? User.where(:id => @@filtered_user_id).first.first_name + '_' : 'all_'
       name = "Orders_for_#{user_name} #{Time.now.utc.strftime('%d-%M-%Y')}.xlsx"
       response.headers['Content-Disposition'] = "attachment; filename*=UTF-8''#{name}"
