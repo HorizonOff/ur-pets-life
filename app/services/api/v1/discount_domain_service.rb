2a3613518879e6d0ff0632ed17645db849007340
Yorkego <yorkego1@ukr.net>
2019-04-24 14:17:23 +0300

    Auto confirm email of domain gmail, hotline, yahoo

--- app/services/api/v1/discount_domain_service.rb
+++ app/services/api/v1/discount_domain_service.rb
@@ -3,17 +3,22 @@ module Api
     class DiscountDomainService
       def initialize(email)
         @email = email
+        @popular_domains = %w[@gmail.com @hotmail.com @yahoo.com].freeze
+        @domain = '@' + email.split("@").last
       end
 
       def dicount_on_email
-        domain = '@' + @email.split("@").last
-        discount = DiscountDomain.find_by(domain: domain)&.discount
+        discount = DiscountDomain.find_by(domain: @domain)&.discount
         discount
+      end
+
+      def is_domain_popular
+        @domain.in?(@popular_domains)
       end
 
       private
 
-      attr_reader :email
+      attr_reader :email, :domain, :popular_domains
     end
   end
 end
