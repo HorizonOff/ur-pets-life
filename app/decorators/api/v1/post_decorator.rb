558f513ecbc10a23f310f3769da6f70a9fa620ab
Yorkego <yorkego1@ukr.net>
2019-05-28 11:34:37 +0300

    Add unread comments to posts

---
+++ app/decorators/api/v1/post_decorator.rb
@@ -0,0 +1,47 @@
+module Api
+  module V1
+    class PostDecorator < ApplicationDecorator
+      decorates :post
+      delegate_all
+
+      def image
+        model.image.file.blank? ? image_hash : model.image
+      end
+
+      def video
+        model.video.file.blank? ? video_hash : model.video
+      end
+
+      def created_at
+        model.created_at.to_i
+      end
+
+      def unread_post_comments_count
+        model.user_posts.find_by(user_id: h.current_user&.id)&.unread_post_comments_count
+      end
+
+      private
+
+      def image_hash
+        {
+          url: model.mobile_image_url,
+          medium: {
+            url: model.mobile_image_url
+          },
+          thumb: {
+            url: model.mobile_image_url
+          }
+        }
+      end
+
+      def video_hash
+        {
+          url: model.mobile_video_url,
+          thumb: {
+            url: nil
+          }
+        }
+      end
+    end
+  end
+end
