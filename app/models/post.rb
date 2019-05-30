819a0db57901ef50eab5cc2512fe76bd598d5f7e
Yorkego <yorkego1@ukr.net>
2019-05-30 13:50:02 +0300

    Small fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,13 +13,26 @@ class Post < ApplicationRecord
   delegate :name, to: :user, prefix: true
   delegate :avatar, to: :user, allow_nil: true
 
-  after_commit :create_user_post, on: :create
+  after_commit :create_media_from_url, :create_user_post, on: :create
 
   def update_counters(user_id)
     user_posts.find_by(user_id: user_id)&.update_column(:unread_post_comments_count, 0)
   end
 
   private
+
+  def create_media_from_url
+    CreateImageWorker.perform_async(id, 'Post') if mobile_image_url.present?
+    CreateVideoWorker.perform_async(id, 'Post') if mobile_video_url.present?
+  end
+
+  def content_should_be_valid
+    errors.add(:base, 'Should be message or video or image') if image.blank? && video.blank? && message.blank?
+  end
+
+  def ome_type_of_media
+    errors.add(:base, 'Only image or video in one post') if image.present? && video.present?
+  end
 
   def create_user_post
     return if user.user_posts.where(post_id: id).any?
