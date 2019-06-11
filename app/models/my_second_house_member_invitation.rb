40ab86f43edbd618f163b65acacebe927f7f5774
Yorkego <yorkego1@ukr.net>
2019-06-11 17:54:38 +0300

    temp

--- app/models/my_second_house_member_invitation.rb
+++ app/models/my_second_house_member_invitation.rb
@@ -1,6 +1,15 @@
 class MySecondHouseMemberInvitation < ApplicationRecord
   after_commit :send_invitation, on: :create
 
+  def self.import(file)
+    spreadsheet = Roo::Spreadsheet.open(file.path)
+    header = spreadsheet.row(1)
+    (2..spreadsheet.last_row).each do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+      create(email: row["Email Address"])
+    end
+  end
+
   private
 
   def send_invitation
