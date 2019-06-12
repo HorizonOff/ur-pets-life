44ee69025cb61f66ec34f08c43c9aac67efc83fa
Yorkego <yorkego1@ukr.net>
2019-06-12 15:08:07 +0300

    Add default email for invitaion

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,4 +1,6 @@
 class InvitationMailer < ApplicationMailer
+  default from: ENV['MY_SECOND_HOUSE_EMAIL']
+
   def invite_my_second_house_member(invitation_id)
     @invitation = MySecondHouseMemberInvitation.find_by(id: invitation_id)
     mail(to: @invitation.email, subject: 'Welcome to app')
