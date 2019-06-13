6681e26eb31e994c565f0898c2bd6aec16a343ce
Yorkego <yorkego1@ukr.net>
2019-06-13 16:55:03 +0300

    Add Amazon SES

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -3,6 +3,9 @@ class InvitationMailer < ApplicationMailer
 
   def invite_my_second_house_member(invitation_id)
     @invitation = MySecondHouseMemberInvitation.find_by(id: invitation_id)
-    mail(to: @invitation.email, subject: 'Welcome to app')
+    delivery_options = { user_name: ENV['AWS_SMTP_USERNAME'],
+                         password: ENV['AWS_SMTP_PASSWORD'],
+                         address: 'email-smtp.us-west-2.amazonaws.com' }
+    mail(to: @invitation.email, subject: 'Welcome to app', delivery_method_options: delivery_options)
   end
 end
