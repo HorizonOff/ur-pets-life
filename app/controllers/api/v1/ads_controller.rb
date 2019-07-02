66147e17675d9b7b222a97d414726732cacded68
Yorkego <yorkego1@ukr.net>
2019-07-02 12:03:41 +0300

    Add counter

--- app/controllers/api/v1/ads_controller.rb
+++ app/controllers/api/v1/ads_controller.rb
@@ -3,6 +3,7 @@ module Api
     class AdsController < Api::BaseController
       def current
         @ad = Ad.active
+        @ad.update_column(:view_count, @ad.view_count + 1)
         render json: { image_url: @ad.image_url }.to_json
       end
     end
