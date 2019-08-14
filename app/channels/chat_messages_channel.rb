51bfe055c154ce4bd44f3bc94c20e19d4e8e2b87
Yorkego <yorkego1@ukr.net>
2019-08-14 16:20:11 +0300

    add action cable

---
+++ app/channels/chat_messages_channel.rb
@@ -0,0 +1,16 @@
+class ChatMessagesChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "chat-#{params['chat_id']}:messages"
+    redis_key = "chat-#{params['chat_id']}"
+    all_ids = JSON.parse($redis.get(redis_key) || '[]')
+    all_ids.push(current_session.id)
+    $redis.set(redis_key, all_ids.reject(&:blank?).to_json)
+  end
+
+  def unsubscribed
+    redis_key = "chat-#{params['chat_id']}"
+    all_ids = JSON.parse($redis.get(redis_key) || '[]')
+    all_ids.delete(current_session.id)
+    $redis.set(redis_key, all_ids.reject(&:blank?).to_json)
+  end
+end
