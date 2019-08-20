4f00eb83720c3bc13de0a53ccfa942175094d3f5
Yorkego <yorkego1@ukr.net>
2019-08-20 14:06:35 +0300

    Add forgency

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,5 +1,6 @@
-$(function() {
-  $('#new_room_message').on('ajax:success', function(a, b,c ) {
+$( document ).on('turbolinks:load', function() {
+  $('#new_chat_message').ajaxSend(function() {
     $(this).find('input[type="text"]').val('');
+    $(this).find('input[type="file"]').val('');
   });
 });
