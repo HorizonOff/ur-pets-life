d0c99547b5dc1699ffed4d04dd403415352d5c99
Yorkego <yorkego1@ukr.net>
2019-08-21 14:37:59 +0300

    Add title to push

--- app/services/push_sending/chat_message_service.rb
+++ app/services/push_sending/chat_message_service.rb
@@ -1,33 +1,36 @@
 module PushSending
   class ChatMessageService < PushSending::BaseService
     def initialize(chat_message, user)
-      @chat_message = ::Api::V1::ChatMessageDecorator.decorate(chat_message)
+      @chat_message = chat_message
       @user = user
+      @chat_message_body = if chat_message.text.present?
+                             chat_message.text
+                           elsif chat_message.photo.present? || chat_message.mobile_photo_url.present?
+                             'Sent you photo'
+                           elsif chat_message.video.present? || chat_message.mobile_video_url.present?
+                             'Sent you video'
+                           end
     end
 
     private
 
-    attr_reader :chat_message, :user, :author
+    attr_reader :chat_message, :chat_message_body, :user, :author
 
     def ios_options
-      { alert: { title: 'UrPetsLife', body: chat_message.text },
+      { alert: { title: 'UrPetsLife', body: chat_message_body },
         sound: 'default',
         badge: ios_badge,
         # unread_notifications_count: unread_notifications_count,
         # unread_messages_count: unread_messages_count,
-        source_type: 'ChatMessage',
-        chat_message: ActiveModelSerializers::SerializableResource.new(
-          chat_message, adapter: :attributes, serializer: ChatMessageSerializer
-        ).as_json }
+        source_type: 'ChatMessage'
+      }
     end
 
     def android_options
       {
-        data: { chat_message: ActiveModelSerializers::SerializableResource.new(
-          chat_message, adapter: :attributes, serializer: ChatMessageSerializer
-        ).as_json,
-                source_type: 'ChatMessage',
+        data: { body: chat_message_body,
                 title: 'UrPetsLife',
+                source_type: 'ChatMessage',
                 badge: android_badge
                 # unread_notifications_count: unread_notifications_count,
                 # unread_messages_count: unread_messages_count
