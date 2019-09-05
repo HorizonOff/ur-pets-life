7b5b236a8a232be58c3d5dcb380210a9e370443f
Yorkego <yorkego1@ukr.net>
2019-09-05 10:47:13 +0300

    Add points to user code

--- app/controllers/api/v1/used_pay_codes_controller.rb
+++ app/controllers/api/v1/used_pay_codes_controller.rb
@@ -8,10 +8,10 @@ module Api
 
       def check_pay_code
         user = User.find_by(pay_code: params[:pay_code])
-        return render_404 if user.blank?
+        return render_422('Code not found') if user.blank?
         return render_422('You can not use your own code') if user == current_user
-        return render_422('Already used 3 times') if user.used_pay_codes.count >= 3
-        return render_422('You already use this code') if current_user.id.in?(user.used_pay_codes&.pluck(:code_user_id))
+        return render_422('This code already used 3 times') if user.used_pay_codes.count >= 3
+        return render_422('Each user can use one code') if UsedPayCodes.where(code_user_id: current_user.id).any?
 
         render json: { message: 'Code checked' }.to_json
       end
--- app/models/used_pay_code.rb
+++ app/models/used_pay_code.rb
@@ -2,4 +2,20 @@ class UsedPayCode < ApplicationRecord
   belongs_to :user
   belongs_to :order
   belongs_to :code_user, class_name: 'User', foreign_key: :code_user_id
+
+  after_commit :create_new_pay_code, :add_redeem_points, on: :create
+
+  private
+
+  def create_new_pay_code
+    return if code_user.pay_code.present?
+
+    code_user.update_column(:pay_code, "%04d" % [code_user.id + 9])
+  end
+
+  def add_redeem_points
+    redeem_p = user.redeem_point
+    redeem_p.update(totalearnedpoints: redeem_p.totalearnedpoints + 30,
+                    totalavailedpoints: redeem_p.totalavailedpoints + 30)
+  end
 end
