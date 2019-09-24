461be5234c8c18ee621d52dc0f4b8a1ac3ee91b8
Igor Kulich <nikita.nizhehorodov@foxtrapp.com>
2019-09-24 19:18:59 +0300

    Small fix

--- app/controllers/admin_panel/orders_controller.rb
+++ app/controllers/admin_panel/orders_controller.rb
@@ -129,14 +129,14 @@ module AdminPanel
     check_for_unregistered_user if @unregistered_user.blank?
     unregistered_user_id = @unregistered_user&.id
     return redirect_to new_admin_panel_order_path, flash: { error: "User must exist!" } if unregistered_user_id.blank? && @user.blank?
-    @location_id = @user&.location.present? ? @user.location.id : create_new_location
+    location_id = @user.present? ? @user.location.id : new_location_id
 
     @order = Order.new(user_id: params['user_id'], unregistered_user_id: unregistered_user_id,
                        RedeemPoints: permitted_redeem_points, Subtotal: @total_price_without_discount,
                        Delivery_Charges: deliveryCharges, shipmenttime: 'with in 7 days', Vat_Charges: vatCharges,
                        Total: total, Order_Status: 1, Payment_Status: paymentStatus,
                        Delivery_Date: params[:Delivery_Date], Order_Notes: params[:Order_Notes],
-                       IsCash: true, is_viewed: false, location_id: @location_id,
+                       IsCash: true, is_viewed: false, location_id: location_id,
                        order_status_flag: 'pending', company_discount: company_discount,
                        is_user_from_company: @is_user_from_company, admin_discount: admin_discount)
 
@@ -430,8 +430,8 @@ module AdminPanel
       OrderMailer.send_low_inventory_alert(itemid).deliver_later
     end
 
-    def create_new_location
-      @location = Location.create(order_params[:location_attributes]).id
+    def new_location_id
+      Location.create(order_params[:location_attributes]).id
     end
 
     def order_params
