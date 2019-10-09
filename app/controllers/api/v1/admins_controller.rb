6d6369903405098d2eb1fb000988880ffb45b6fc
Igor Kulich <nikita.nizhehorodov@foxtrapp.com>
2019-10-09 15:05:41 +0300

    Restore route

--- app/controllers/api/v1/admins_controller.rb
+++ app/controllers/api/v1/admins_controller.rb
@@ -1,17 +1,10 @@
 module Api
   module V1
     class AdminsController < Api::BaseController
+
       def index
-        @admins = Admin.where(role: "driver")
-        if @admins.nil? or @admins.empty?
-          render :json => {
-              Message: 'No Drivers found'
-          }
-        else
-          render json: @admins.as_json(
-            :only => [:id, :name]
-          )
-        end
+        @admin = Order.where(role: params[:role])
+        render json: @admin.as_json(only: [:id, :name])
       end
     end
   end
