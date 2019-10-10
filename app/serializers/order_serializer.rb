884f87e038a028623727f697cff2644c2ad96fa0
Igor Kulich <nikita.nizhehorodov@foxtrapp.com>
2019-10-10 17:41:30 +0300

    New params

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,4 +1,8 @@
 class OrderSerializer < ActiveModel::Serializer
   attributes :id, :Delivery_Date, :Order_Notes, :Delivery_Charges, :Vat_Charges, :Subtotal, :Total, :IsCash,
-             :Order_Status, :Payment_Status, :order_status_flag
+             :Order_Status, :Payment_Status, :order_status_flag, :driver_name
+  
+  def driver_name
+    self.driver.name
+  end
 end
